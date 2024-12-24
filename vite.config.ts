40d22db8226d39932e56f4a250c675dd92d7c661
Shaxriyor696 <shaxriyorpirnazarov8@gmail.com>
2024-12-24 23:57:04 +0500

    Amogus

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://shaxriyor696.github.io/alphabet_drag_and_drop",
   plugins: [react()],
 })
